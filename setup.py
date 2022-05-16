d866da742d170152bca1cb6d2aafe19a76faa066
Aditya Shylesh <aditya.shylesh@verizonmedia.com>
2022-05-16 00:10:18 -0500

    Updating package metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_desc = fh.read()
 setup(
     name='leddite',
     version='0.1.9',
+    author="Aditya Shylesh",
+    author_email="development-mails@adishy.com",
+    description="leddite lets you create your own art with Neopixel displays easily",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adishy/leddite",
+    project_url = {
+        "Bug Tracker": "https://github.com/adishy/leddite.issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=['leddite'],
     include_package_data=True,
     entry_points={
@@ -22,4 +38,5 @@ setup(
         'rich',
         "rpi_ws281x;platform_system=='Linux'"
     ],
+    python_requires=">=3.6",
 )
